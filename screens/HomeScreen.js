2123bfaa5fb5eb8234298c66fb3b000215c9da02
LathithaKunana <lathitha.kunana@younglings.africa>
2024-02-28 16:25:09 +0200

    created a new component movieList

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react'
-import { Platform, SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
+import React, { Component, useState } from 'react'
+import { Platform, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import { styles } from '../theme';
+import TrendingMovies from '../components/trendingMovies';
 
 const ios =  Platform.OS === 'ios';
 export default function HomeScreen() {
+    const [trending , setTrending] = useState([1,2,3,4])
 
 
     return(
-    <View className = "flex-1 bg-neutral-800 "> 
+    <View className = "flex-1 bg-neutral-800 ">
+
         {/* search bar and logo */}
 
-        <SafeAreaView className = {ios? "-mb-2" : "mt-8"}>
+        <SafeAreaView className = {ios? "mb-5" : "mt-8"}>
             <StatusBar style="light" />
             <View className="flex-row justify-between items-center mx-4">
                 <Bars3CenterLeftIcon size={30} strokeWidth={2} color="#fff"/>
             <Text 
-                className= "text-white text-3xl font-bold ">Movies
+                className= "text-white text-3xl font-bold "><Text style ={styles.text}>M</Text>ovies
             </Text>
             <TouchableOpacity>
                 <MagnifyingGlassIcon onPress={()=>{alert("Search Functionality Coming Soon")}} size={30} color="#fff"/>
             </TouchableOpacity>
             </View>
         </SafeAreaView>
+
+        {/* trending movies*/}
+
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingBottom: 10}}
+        >
+            {/* trending movies carousel*/}
+            <TrendingMovies data={trending}/>
+        </ScrollView>
+
+
     </View>
     )
     
