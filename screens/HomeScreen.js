b94acc6ecaeb159f99c5f91c4a5e359c94a0d1b8
LathithaKunana <lathitha.kunana@younglings.africa>
2024-02-28 13:30:36 +0200

    started new project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+
+export default function HomeScreen() {
+    return(
+    <SafeAreaView>
+        <Text>HomeScreen</Text>
+    </SafeAreaView>
+    )
+    
+}
